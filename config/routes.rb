408b4609e21fba570e39bdffd4048e857efef365
Harold Kimball <spht.lhar@gmail.com>
2016-12-22 02:20:17 +0000

    rename routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#index'
+  root 'pages#home'
   get 'about' => 'pages#about'
   get 'news' => 'pages#news'
   get 'patch-notes' => 'pages#patch'
