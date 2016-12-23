d68d5bd9f9c1de442a11727f9bbd1dda6266dc08
Harold Kimball <spht.lhar@gmail.com>
2016-12-23 08:34:45 +0000

    cant get images to load retry 15

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,6 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.serve_static_assets = true
-    config.assets.compile = true
-    
     config.exceptions_app = self.routes
   end
   
