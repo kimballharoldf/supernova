edcd55a9ff89718811556be90051661173addbef
Harold Kimball <spht.lhar@gmail.com>
2017-01-12 16:16:46 +0000

    Updated Server Status indicator pref

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,13 @@ class PagesController < ApplicationController
   
   def home
     require 'net/ping'
-    host = 'supernovaffxi.ddns.net'
+    host = 'login.supernovaffxi.com'
     @current_time = Time.zone.now.in_time_zone("Eastern Time (US & Canada)")
     @post = Post.last
     
     p1 = Net::Ping::TCP.new(host, 54230)
-    p2 = Net::Ping::TCP.new(host, 54230)
-    p3 = Net::Ping::TCP.new(host, 54231)
-    p4 = Net::Ping::TCP.new(host, 54001)
+    p2 = Net::Ping::TCP.new(host, 54231)
+    p3 = Net::Ping::TCP.new(host, 54001)
     
     if p1.ping? == true
       @world = "Online"
@@ -17,7 +16,7 @@ class PagesController < ApplicationController
       @world = "Offline"
     end
     
-    if p2.ping? == true && p3.ping? && p4.ping?
+    if p2.ping? == true && p3.ping? == true
       @login = "Online"
     else
       @login = "Offline"
